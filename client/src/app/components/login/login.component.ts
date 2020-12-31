0bd5505e00ef4a4c8a477929bd9e161ebada82a5
ulrichdah <ulrich.dah-achinanon@polymtl.ca>
2020-12-30 19:32:43 -0500

    Removed some useless part from client

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  User: any = ['Super Admin', 'Author', 'Reader'];
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class LoginComponent {
 
 }
