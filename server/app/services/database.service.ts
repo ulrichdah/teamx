6995492de33aa8ca94229b4cd32acd99f7a0716d
ulrichdah <ulrich.dah-achinanon@polymtl.ca>
2021-01-09 01:20:40 -0500

    Removed information

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,8 +1,7 @@
 import { injectable } from 'inversify';
 import * as mongodb from 'mongodb';
 
-const DB_PASSWORD = 'OilRHNKOLrJcNi9s';
-const DB_NAME = 'teamx_0';
+
 const uri = 'mongodb+srv://teamx-user0:' + DB_PASSWORD + '@teamxcluster0.ktn55.mongodb.net/' + DB_NAME + '?retryWrites=true&w=majority';
 
 @injectable()
