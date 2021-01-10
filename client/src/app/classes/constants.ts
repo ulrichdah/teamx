54bbf026dc1f70d73376c6589b2da13febf52c3d
ulrichdah <ulrich.dah-achinanon@polymtl.ca>
2021-01-09 22:36:12 -0500

    User registration UI done

--- client/src/app/classes/constants.ts
+++ client/src/app/classes/constants.ts
@@ -3,5 +3,8 @@ export enum AccountType {
     INCOMPLETE_TEAM = 'Groupe incomplet'
 }
 
+export const MIN_USERNAME_LENGTH = 3;
+export const MAX_USERNAME_LENGTH = 15;
+
 export const MIN_PASSWORD_LENGTH = 6;
-export const MAX_PASSWORD_LENGTH = 20;
+export const MAX_PASSWORD_LENGTH = 30;
--- client/src/app/components/user-registration/user-registration.component.ts
+++ client/src/app/components/user-registration/user-registration.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-import { AccountType, MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH } from 'src/app/classes/constants';
+import { AccountType, MAX_PASSWORD_LENGTH, MAX_USERNAME_LENGTH, MIN_PASSWORD_LENGTH, MIN_USERNAME_LENGTH } from 'src/app/classes/constants';
+import { Student } from 'src/app/classes/course';
 
 @Component({
   selector: 'app-user-registration',
@@ -25,23 +26,45 @@ export class UserRegistrationComponent implements OnInit {
 
   ngOnInit(): void {
     this.registrationForm = this.fb.group({
+      accountType:['', Validators.required],
+      teamName: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', Validators.required, Validators.minLength(MIN_PASSWORD_LENGTH), Validators.maxLength(MAX_PASSWORD_LENGTH)],
-      confirmPassword: ['', Validators.required,  Validators.minLength(MIN_PASSWORD_LENGTH), Validators.maxLength(MAX_PASSWORD_LENGTH)],
-      email: ['', Validators.required, Validators.email],
+      username: ['', [Validators.required, Validators.minLength(MIN_USERNAME_LENGTH), Validators.maxLength(MAX_USERNAME_LENGTH)]],
+      password: ['', [Validators.required, Validators.minLength(MIN_PASSWORD_LENGTH), Validators.maxLength(MAX_PASSWORD_LENGTH)]],
+      confirmPassword: ['', [Validators.required,  Validators.minLength(MIN_PASSWORD_LENGTH), Validators.maxLength(MAX_PASSWORD_LENGTH)]],
+      email: ['', [Validators.required, Validators.email]],
       facebook: [''],
-      courses: this.fb.array([])
+      courses: this.fb.array([this.fb.group({
+        courseName:['', Validators.required],
+        experiences:[''],
+        goals:['', Validators.required],
+        grade:[''],
+        availabilities:[''],
+      })])
     });
+    this.observeCourseField();
   }
 
   get courses(): FormArray {
     return this.registrationForm.get('courses') as FormArray;
   }
 
+  onSelectionChange(value: string): void {
+    if (value === this.accounts[0]) {
+      this.registrationForm.removeControl('teamName');
+      this.registrationForm.addControl('firstName', new FormControl('', Validators.required));
+      this.registrationForm.addControl('lastName', new FormControl('', Validators.required));
+    } else {
+      this.registrationForm.removeControl('firstName');
+      this.registrationForm.removeControl('lastName');
+      this.registrationForm.addControl('teamName', new FormControl('', Validators.required));
+    }
+  }
+
   onSubmit(): void {
-    console.log('toto');
+    const toto: Student = this.registrationForm.value as Student;
+    console.log(toto);
   }
 
   addCourse(): void {
@@ -52,7 +75,6 @@ export class UserRegistrationComponent implements OnInit {
       grade:[''],
       availabilities:[''],
     }));
-    this.observeCourseField();
   }
 
   deleteCourse(index: number): void {
@@ -61,6 +83,24 @@ export class UserRegistrationComponent implements OnInit {
 
   displayFn(course: string): string {
     return course && course ? course : '';
+  }
+
+  isFieldInvalid(fieldName: string, subFieldName?: string, index?: number): boolean | null {
+    const formField = subFieldName && index != undefined ? this.courses.at(index).get(subFieldName) : this.registrationForm.get(fieldName);
+    return formField && formField.invalid && formField.errors && (formField.dirty || formField.touched);
+  }
+
+  isLengthInvalid(fieldName: string): boolean {
+    const formField = this.registrationForm.get(fieldName);
+    return formField && formField.errors && (formField.errors.minlength || formField.errors.maxlength);
+  }
+
+  isPasswordConfirmed(): boolean | null {
+    const passwordField = this.registrationForm.get('password');
+    const confirmPasswordField = this.registrationForm.get('confirmPassword');
+    return passwordField && confirmPasswordField &&
+    (passwordField.touched || passwordField.dirty) && (confirmPasswordField.touched || confirmPasswordField.dirty) &&
+    passwordField.value === confirmPasswordField.value;
   }
 
   private _filter(name: string): string[] {
